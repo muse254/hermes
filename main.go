ca5eef48659d8f2717e660745c349dbbdd907163
muse254 <fanosoro@gmail.com>
2019-08-24 00:28:33 +0300

    bugs

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"errors"
 	"flag"
+	"fmt"
 	"github.com/rjeczalik/notify"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
-var (
-	projectDir = flag.String("project", "",
-		"it point to the project directory to watch for changes")
-	mainPath = flag.String("main", "",
-		"it points to the main.go file from where the program is run")
-	gorun = flag.Bool("gorun", false,
-		"if true it does a 'go run .../.../main.go for every change made")
-	gotest = flag.Bool("gotest", false,
-		"if true it does a 'go test' for every change made")
-	gobuild = flag.Bool("gobuild", false,
-		"if true does a 'go build' for every change made")
-)
+var projectDir = flag.String("project", "",
+	"it points to the project directory to watch for changes")
 
 func main() {
+	mainPath := flag.String("main", "",
+		"it points to the main.go file from where the program is run")
+	gorun := flag.Bool("gorun", false,
+		"if true it does a 'go run ...' for every change made")
+	gotest := flag.Bool("gotest", false,
+		"if true it does a 'go test' for every change made")
+	gobuild := flag.Bool("gobuild", false,
+		"if true does a 'go build' for every change made")
 	flag.Parse()
+
 	if *projectDir == "" {
 		flag.PrintDefaults()
 		return
@@ -32,9 +32,6 @@ func main() {
 		filePath, err := lookForMain(*projectDir)
 		if err != nil {
 			errLogger(err)
-		}
-		if filePath == "" {
-			errLogger(errors.New("main.go file was not found"))
 		}
 		*mainPath = filePath
 	}
@@ -45,30 +42,56 @@ func main() {
 
 	listen := make(chan notify.EventInfo, 1)
 	defer close(listen)
+
+	subs := strings.SplitN(*projectDir, "/", -1)
+	projectName := subs[len(subs)-1]
 	for {
 		if *gorun {
-			pre := cmd("go", "run", *mainPath)
 			errLogger(notify.Watch(*projectDir, listen, notify.All))
+			pre := cmd("go", "run", *mainPath)
+			go func() {
+				fmt.Printf("hermes: running %s",projectName)
+				errLogger(pre.Start())
+				pre.Wait()
+			}()
 			<-listen
-			errLogger(pre.Kill())
+			err := pre.Process.Signal(os.Kill)
+			errLogger(err)
+			log.Println("hermes: rerunning...")
 		} else if *gotest {
 			pre := cmd("go", "test")
 			errLogger(notify.Watch(*projectDir, listen, notify.All))
+			go func() {
+				fmt.Printf("hermes: testing %s",projectName)
+				errLogger(pre.Start())
+				pre.Wait()
+			}()
 			<-listen
-			errLogger(pre.Kill())
+			err := pre.Process.Signal(os.Kill)
+			errLogger(err)
+			log.Println("hermes: retesting...")
 		} else if *gobuild {
 			//todo
-			pre := cmd("go", "build", "")
+			pre := cmd("go", "build")
 			errLogger(notify.Watch(*projectDir, listen, notify.All))
+			go func() {
+				fmt.Printf("hermes: building %s",projectName)
+				errLogger(pre.Start())
+				pre.Wait()
+				// run the build: ./projectName
+				cmd("", projectName)
+			}()
 			<-listen
-			errLogger(pre.Kill())
+			err := pre.Process.Signal(os.Kill)
+			errLogger(err)
+			log.Println("hermes: rebuilding")
 		}
 
 	}
 }
 
-// lookForMain takes a folder and looks for main.go file
-// if successful it returns mainFile, nil else it returns "", dir (if any) or nil
+// lookForMain looks for main.go file in the directory given
+// returning the main file if it was found and err, if any
 func lookForMain(path string) (mainFile string, err error) {
 	folder, err := os.Open(path)
 	if err != nil {
@@ -97,25 +120,28 @@ func lookForMain(path string) (mainFile string, err error) {
 			if err != nil {
 				return "", nil
 			}
-			if mainFile != "" {
-				return
-			}
 		}
 	}
 	return
 }
 
-func cmd(name string, arg ...string) *os.Process {
+// cmd initialises the command
+func cmd(name string, arg ...string) *exec.Cmd {
 	cmd := exec.Command(name, arg...)
 	cmd.Stdout = os.Stdout
+	cmd.Dir = *projectDir
 	cmd.Stderr = os.Stderr
 
-	errLogger(cmd.Run())
-	return cmd.Process
+	return cmd
 }
 
+//
 func errLogger(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+// BUGS
+// processes leak after hermes has returned
+// should release resources of previous os.Process
