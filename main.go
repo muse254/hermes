6cd324e578a7ef79d943bb5ab593b1c08d8037f4
muse254 <fanosoro@gmail.com>
2019-08-25 09:22:08 +0300

    still working on cleaning up resources

--- main.go
+++ main.go
@@ -7,13 +7,19 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"sync"
 )
 
-var projectDir = flag.String("project", "",
-	"it points to the project directory to watch for changes")
+type process struct {
+	process  *exec.Cmd
+	mutex      sync.Mutex
+}
 
 func main() {
+	projectDir := flag.String("project", "",
+		"it points to the project directory to watch for changes")
 	mainPath := flag.String("main", "",
 		"it points to the main.go file from where the program is run")
 	gorun := flag.Bool("gorun", false,
@@ -36,33 +42,71 @@ func main() {
 		*mainPath = filePath
 	}
 
+	// use gorun as default value
 	if *gorun == false && *gotest == false && *gobuild == false {
 		*gorun = true
 	}
 
-	listen := make(chan notify.EventInfo, 1)
-	defer close(listen)
-
 	subs := strings.SplitN(*projectDir, "/", -1)
 	projectName := subs[len(subs)-1]
+
+	//errInterrupt := errors.New(fmt.Sprintf("%s has received SIGINT",projectName))
+
+	wg := sync.WaitGroup{}
+	listen := make(chan notify.EventInfo, 1)
+	defer close(listen)
+	interrupt := make(chan os.Signal, 1)
+	defer close(interrupt)
+	wait := make(chan error, 1)
+	defer close(wait)
+
 	for {
 		if *gorun {
 			errLogger(notify.Watch(*projectDir, listen, notify.All))
-			pre := cmd("go", "run", *mainPath)
+			signal.Notify(interrupt, os.Interrupt)
+			pre := cmd(*projectDir, "go", "run", *mainPath)
+			proc := &process{
+				 pre,
+			    sync.Mutex{},
+			}
+
+			wg.Add(1)
 			go func() {
-				fmt.Printf("hermes: running %s",projectName)
-				errLogger(pre.Start())
-				pre.Wait()
+
+				defer wg.Done()
+				fmt.Printf("hermes: running %s ...\n", projectName)
+				errLogger(proc.process.Start())
+
+				//go procWait(wait, proc)
+
+				select {
+				case <-listen:
+					clean(proc)
+					break
+				case <-interrupt:
+					clean(proc)
+					break
+				case err := <-wait:
+					if err == nil {
+						fmt.Printf("hermes: %s run was successful\n", projectName)
+					} else {
+						if exitError, ok := err.(*exec.ExitError); ok {
+							code := exitError.ExitCode()
+							fmt.Printf("hermes: %s run was unsuccessful, exit code %d\n", projectName, code)
+						}
+					}
+					break
+				}
+				return
 			}()
-			<-listen
-			err := pre.Process.Signal(os.Kill)
-			errLogger(err)
-			log.Println("hermes: rerunning...")
+			wg.Wait()
+			log.Printf("hermes: rerunning %s ...\n", projectName)
+
 		} else if *gotest {
-			pre := cmd("go", "test")
+			pre := cmd(*projectDir, "go", "test")
 			errLogger(notify.Watch(*projectDir, listen, notify.All))
 			go func() {
-				fmt.Printf("hermes: testing %s",projectName)
+				fmt.Printf("hermes: testing %s\n", projectName)
 				errLogger(pre.Start())
 				pre.Wait()
 			}()
@@ -72,10 +116,10 @@ func main() {
 			log.Println("hermes: retesting...")
 		} else if *gobuild {
 			//todo
-			pre := cmd("go", "build")
+			pre := cmd(*projectDir, "go", "build")
 			errLogger(notify.Watch(*projectDir, listen, notify.All))
 			go func() {
-				fmt.Printf("hermes: building %s",projectName)
+				fmt.Printf("hermes: building %s\n", projectName)
 				errLogger(pre.Start())
 				pre.Wait()
 				// run the build: ./projectName
@@ -88,6 +132,21 @@ func main() {
 		}
 
 	}
+}
+
+// procWait waits for the process to complete, If it completed
+// without interrupt being called it writes to the chan error
+func procWait(err chan error, proc *process) {
+	someErr := proc.process.Wait()
+	err <- someErr
+}
+
+// clean releases resources of the process and then does a Kill
+func clean(proc *process)  {
+	proc.mutex.Lock()
+	proc.process.Process.Release()
+	proc.process.Process.Kill()
+	proc.mutex.Unlock()
 }
 
 // lookForMain looks for main.go file in the directory given
@@ -126,10 +185,10 @@ func lookForMain(path string) (mainFile string, err error) {
 }
 
 // cmd initialises the command
-func cmd(name string, arg ...string) *exec.Cmd {
+func cmd(projectDir string, name string, arg ...string) *exec.Cmd {
 	cmd := exec.Command(name, arg...)
 	cmd.Stdout = os.Stdout
-	cmd.Dir = *projectDir
+	cmd.Dir = projectDir
 	cmd.Stderr = os.Stderr
 
 	return cmd
